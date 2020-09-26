7d479b261585e11e6fd55bdb76b9b17d20f335fc
abdlalisalmi <abdlalisalmi@gmail.com>
2020-09-26 09:52:39 +0100

    first commit

--- truly/settings.py
+++ truly/settings.py
@@ -1,4 +1,6 @@
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -8,10 +10,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ow3x%prip7zj62yp3v@anj$r31=8qjnrb&(#*io!*3naaq4qtc'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
